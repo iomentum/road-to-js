5deeef3c73679396d9e7336dadaedc7dd3d2dd6f
Rabire <rabireh@outlook.fr>
2021-04-27 17:38:32 +0200

    use effect

--- simple-react-app/src/component/RegisterForm/RegisterForm.jsx
+++ simple-react-app/src/component/RegisterForm/RegisterForm.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./RegisterForm.css";
 import Field from "../Fields/Field";
 
 const RegisterForm = () => {
-  const [fullName, setFullName] = useState("");
+  const [, setFullName] = useState("");
   const [age, setAge] = useState(0);
-  const [favComics, setFavComics] = useState("");
-  const [favCarBrand, setFavCarBrand] = useState("");
+  const [, setFavComics] = useState("");
+  const [, setFavCarBrand] = useState("");
+  const [gender, setGender] = useState(undefined);
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    if (gender === "male") return setColor("dodgerblue");
+    else if (gender === "female") return setColor("pink");
+
+    return setColor("black");
+  }, [gender]);
 
   return (
     <form>
       <div className="container">
-        <h1>Parlez-nous un peu de vous</h1>
+        <h1 style={{ color: color }}>Parlez-nous un peu de vous</h1>
         <p>Completez les champs ci-dessous</p>
         <hr />
-
         <Field label="Nom complet" setField={setFullName} />
         <Field label="Age" type="number" setField={setAge} />
-
         {age ? (
           age >= 18 ? (
             <Field label="Marque de voiture préféré" setField={setFavComics} />
@@ -27,6 +34,18 @@ const RegisterForm = () => {
         ) : (
           []
         )}
+
+        <label for="email">
+          <b>Sexe</b>
+        </label>
+        <select
+          name="gender"
+          onChange={(event) => setGender(event.target.value)}
+        >
+          <option value="">--Please choose an option--</option>
+          <option value="male">Homme</option>
+          <option value="female">Femme</option>
+        </select>
 
         <button type="submit" className="signupbtn">
           S'inscrire
